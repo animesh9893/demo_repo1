6cbad790d861829a3358cdd9cecdab2b8eda6164
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-05-01 01:42:23 +0530

    first'

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"encoding/json"
+)
+
+type Post struct {
+  Message string `json:"Message"`
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w,"Hello Animesh\n");
+}
+
+func HomeGetHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	var p Post
+	p.Message = "Hello Unknown"
+	json.NewEncoder(w).Encode(p)
+}
+
+func HomePostHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Access-Control-Allow-Origin","*")
+	var p Post
+	decoder := json.NewDecoder(r.Body)
+	err:= decoder.Decode(&p)
+	if err!=nil{
+		p.Message = "Something wrong"
+	}
+	p.Message = "All Good"
+
+	json.NewEncoder(w).Encode(p)
+}
+
+func main(){
+	r:= mux.NewRouter()
+	r.HandleFunc("/",HomeHandler)
+	r.HandleFunc("/home",HomeGetHandler).Methods("GET")
+	r.HandleFunc("/home",HomePostHandler).Methods("POST")
+	http.ListenAndServe(":8000",r)
+}
